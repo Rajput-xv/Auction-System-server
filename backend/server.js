3519a0e4b360ef038739ca088005c29473c3d445
Yash Verma <114978813+Rajput-xv@users.noreply.github.com>
2025-04-17 19:35:03 +0530

    update

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const router = express.Router();
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(
+	cors({
+		origin: ["https://auction-system-sigma.vercel.app", "http://localhost:5173"],
+		methods: ["GET", "PUT", "POST", "DELETE"],
+		credentials: true,
+	})
+);
+app.use(router);
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/auctions", require("./routes/auctionRoutes"));
+app.use("/api/bids", require("./routes/bidRoutes"));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
